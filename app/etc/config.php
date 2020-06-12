74070afce5a4dea1bc28e5915360d45b76f997ab
Denis <ermakov@outlook.com>
2020-06-12 14:03:53 +0300

    Install BSS MegaMenu

--- app/etc/config.php
+++ app/etc/config.php
@@ -235,6 +235,7 @@ return [
         'Amazon_Core' => 1,
         'Amazon_Login' => 1,
         'Amazon_Payment' => 1,
+        'Bss_Megamenu' => 1,
         'Dotdigitalgroup_Email' => 1,
         'Dotdigitalgroup_Chat' => 1,
         'Klarna_Core' => 1,
